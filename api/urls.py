e05582f01c59ea76cebb7b7452c59eecca7e3479
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-08-28 16:53:17 +0530

    update

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('master.urls')),
-    path('user1/', include('user.urls')),
+    path('user12/', include('user.urls')),
     # path('user/', include('user.urls')),
 ]
