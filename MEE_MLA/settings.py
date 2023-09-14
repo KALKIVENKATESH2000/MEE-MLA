32e4ce0572ff5d1ae42edd057b19078e6e67437e
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-09-14 17:33:00 +0530

    mysql setup

--- MEE_MLA/settings.py
+++ MEE_MLA/settings.py
@@ -128,24 +128,36 @@ WSGI_APPLICATION = 'MEE_MLA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# LOCAL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mee_mla',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+# PRODUCTION
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mee_mla',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'mee_mla',
+        'PASSWORD': 'Password@123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
